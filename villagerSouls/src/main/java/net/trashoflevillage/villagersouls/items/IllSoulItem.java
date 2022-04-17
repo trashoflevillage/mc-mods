f9434b59272716d43c77b0f737cfa1196bf46e76
trashoflevillage <uhhnokthxbye@gmail.com>
2022-04-17 12:18:18 -0400

    + Item Tooltips

--- villagerSouls/src/main/java/net/trashoflevillage/villagersouls/items/IllSoulItem.java
+++ villagerSouls/src/main/java/net/trashoflevillage/villagersouls/items/IllSoulItem.java
@@ -1,17 +1,27 @@
 package net.trashoflevillage.villagersouls.items;
 
-import net.minecraft.world.InteractionResult;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+import java.util.List;
 
 public class IllSoulItem extends Item {
     public IllSoulItem() {
         super(new Item.Properties().tab(CreativeModeTab.TAB_MISC));
     }
 
-    public InteractionResult use(UseOnContext pContext) {
-        return super.useOn(pContext);
+    // @Nullable means that the parameter might be null.
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(new TranslatableComponent("item.villagersouls.ill_soul.desc"));
     }
+
 }
--- villagerSouls/src/main/java/net/trashoflevillage/villagersouls/items/PureSoulItem.java
+++ villagerSouls/src/main/java/net/trashoflevillage/villagersouls/items/PureSoulItem.java
@@ -1,12 +1,24 @@
 package net.trashoflevillage.villagersouls.items;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PureSoulItem extends Item {
     public PureSoulItem() {
         super(new Item.Properties().tab(CreativeModeTab.TAB_MISC));
+    }
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(new TranslatableComponent("item.villagersouls.pure_soul.desc"));
     }
 
 }
